84643687ad2495be2fa5545336c2d872854ddd56
IndahKusumaPutri <106747173+IndahKusumaPutri@users.noreply.github.com>
2022-09-27 10:48:39 +0700

    regisevent

--- web.blade.php
+++ web.blade.php
@@ -4,3 +4,4 @@ Route::post('/regisevent/store','RegiseventController@store');
 Route::post('/regisevent/edit/{id_regisevent}','RegiseventController@update');
 Route::get('/cetakregisevent','RegiseventController@cetakRegisevent');
 Route::get('/api/fetchnamepasien', 'RegiseventController@fetchName');
+
